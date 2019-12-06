1084aeb96391636755d39ccc094804fcb458366e
ahmad759 <56153728+ahmad759@users.noreply.github.com>
2019-12-06 17:19:16 +0700

    hai

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import requests
 
 response = requests.get (url)
 print (response)
+
+if response.status_code == 200:
+    print ('Koneksi berhasil')
+
+    url_api = f'{url}api/v1/stats/'
+    response = requests.get (url_api)
+    if response.status_code == 200:
+        import json
+        data = json.loads (response.text)
+        data_terakhir = data[len(data) - 1 ]
+
+        suhu = data_terakhir ['suhu']
+        humidity = data_terakhir ['humidity']
+        print (f'Hasil Percobaan sensor : suhu = {suhu}, humidity = {humidity}')
